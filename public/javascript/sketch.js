b1c6732a597658c2de79a8c75ff2711b215fdacd
Millieon Hu <38780746+Millieon@users.noreply.github.com>
2023-02-06 14:54:39 +0000

    change2

--- public/javascript/sketch.js
+++ public/javascript/sketch.js
@@ -1,6 +1,10 @@
+
+
+
 let canvas;
 
 let displayState = 0;
+let a;
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
@@ -11,7 +15,12 @@ function setup() {
 function draw() {
   background(200,200,250);
   
-  circle(width/2,height/2,width/6);
+  //circle(width/2,height/2,width/6);
+  textSize(32);
+  text('Making of my own personal google map data',width/2,height/2);
+  a = createA('https://github.com/Millieon/data_art/blob/main/cali.json', 'link to json data');
+  a.position(width/2, height/2+10);
+
 
 }
 
